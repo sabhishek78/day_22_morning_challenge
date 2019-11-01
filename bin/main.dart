03cdd1f7f28f40ced6d21af083197db47ba1dba4
McLarenCollege <hello@mclarencollege.in>
2019-11-01 08:30:49 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,47 @@
+// Challenge 1
+// Write test cases for the challenges including boundary conditions
+
+// Challenge 2
+// Minesweeper Number of Neighbouring Mines
+// Create a function that takes an array representation of a Minesweeper board,
+// and returns another board where the value of each cell is the amount of its neighbouring mines.
+// Examples
+//  The input may look like this:
+//
+//  [
+//    [0, 1, 0, 0],
+//    [0, 0, 1, 0],
+//    [0, 1, 0, 1],
+//    [1, 1, 0, 0],
+//  ]
+//  The 0 represents an empty space . The 1 represents a mine.
+//
+//  You will have to replace each mine with a 9 and each empty space with the
+//  number of adjacent mines, the output will look like this:
+//
+//  [
+//    [1, 9, 2, 1],
+//    [2, 3, 9, 2],
+//    [3, 9, 4, 9],
+//    [9, 9, 3, 1],
+//  ]
+
+// Challenge 3
+// Group in Order
+// Create a function that groups an array of numbers based on a size parameter. The size represents the maximum length of each sub-array.
+//
+//  [1, 2, 3, 4, 5, 6], 3
+//  [[1, 3, 5], [2, 4, 6]]
+/// Divide array into groups of size 3.
+//
+//  [1, 2, 3, 4, 5, 6], 2
+//  [[1, 4], [2, 5], [3, 6]]
+/// Divide array into groups of size 2.
+//
+//  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4
+//  [[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9]]
+/// "Leftover" arrays are okay.
+
+main() {
+
+}
